fd4624db499df3a73151a96926817c419f6f4888
alireza esmaeili <alirezaesmaeili420@gmail.com>
2023-12-15 17:44:46 +0330

    Update IDS.py

--- IDS.py
+++ IDS.py
@@ -1,22 +1,31 @@
-class IDS:
-    def __init__(self, initial_state, max_depth):
-        self.initial_state = initial_state
-        self.max_depth = max_depth
-        self.visited = set()
+def ids(matrix, start, goal, max_depth):
+    rows, cols = len(matrix), len(matrix[0])
 
-    def search(self):
-        for depth in range(self.max_depth + 1):
-            if self._dfs(self.initial_state, depth):
-                return
+    def dfs_recursive(current_state, depth):
+        if current_state == goal:
+            return True 
 
-    def _dfs(self, current_state, depth):
         if depth == 0:
-            return
+            return False
 
-        if current_state not in self.visited:
-            self.visited.add(current_state)
+        x, y = current_state
+        successors = get_successors(matrix, x, y)
 
-            
+        for successor in successors:
+            if dfs_recursive(successor, depth - 1):
+                return True
 
-            for successor_state in successor(current_state):
-                self._dfs(successor_state, depth - 1)
+        return False
+
+    for depth in range(max_depth + 1):
+        if dfs_recursive(start, depth):
+            return True
+
+    return False  
+
+
+result = ids(matrix_example, start_position, goal_position, max_depth=5)
+if result:
+    print()
+else:
+    print()
